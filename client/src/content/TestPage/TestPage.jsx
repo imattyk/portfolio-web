7cb5ad6823f6b0e51a6324d736eaa663a57d15db
Matej Komlóši <matej.komlosi@gmail.com>
2022-01-28 16:58:07 +0100

    tutorial 1 13 00

--- client/src/content/TestPage/TestPage.jsx
+++ client/src/content/TestPage/TestPage.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
+import { Button, Tag, UnorderedList, ListItem } from "carbon-components-react";
 
 class TestPage extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  styles = {
+    fontSize: "20px",
+    fontWeight: "bold",
+    marginRight: "1rem",
+  };
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this.state.count);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <>
-        <h1>Hello world</h1>
+        <Tag style={this.styles} type={this.getTagColors()} size="sm">
+          {this.formatCount()}
+        </Tag>
+        <Button onClick={this.handleIncrement} size="sm">
+          Increment
+        </Button>
+        {this.renderTags()}
       </>
     );
+  }
+
+  renderTags() {
+    if (this.state.tags.length !== 0) {
+      return (
+        <UnorderedList>
+          {this.state.tags.map((tag) => (
+            <ListItem key={tag}>{tag}</ListItem>
+          ))}
+        </UnorderedList>
+      );
+    }
+  }
+
+  getTagColors() {
+    let colors = "";
+    colors += this.state.count === 0 ? "red" : "blue";
+    return colors;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
