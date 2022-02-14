e7c5c42842b5009faf0c9b27f8ad86b7fcb1c076
Matej Komlóši <matej.komlosi@gmail.com>
2022-02-14 13:52:51 +0100

    updated grammar

--- client/src/components/TimeLine/TimeLine.js
+++ client/src/components/TimeLine/TimeLine.js
@@ -67,27 +67,27 @@ const Timeline = () => {
     <Section id="bio">
       <SectionTitle>Bio</SectionTitle>
       <SectionText className="bio-text">
-        24 rokov, pracovne Web Developer, skoro absolvent FEI STU v Bratislave,
-        pochádzam z Vrábel, malého mesta pri Nitre. Momentálne bývam v
+        24 rokov, pracovne Web Developer, skoro absolvent FEI STU v Bratislave.
+        Pochádzam z Vrábel, malého mesta pri Nitre. Momentálne bývam v
         Bratislave. Už od šiestich rokov, čo ma mamka posadila za gitaru na
         krúžku v škole sa venujem hudbe. Klavír som začal na umeleckej škole od
         ôsmich, bicie od trinástich a nejak som sa dostal aj ku spevu. Takto sa
-        to nabaľovalo az ku hudobnej produkcii a mixáži prvých songov kapely Not
-        Yet Awake (nájdete ju na spotify/youtube). Toto bol môj život asi do
+        to nabaľovalo až ku hudobnej produkcii a mixáži prvých pesničiek kapely
+        Not Yet Awake (nájdete ju na spotify/youtube). Toto bol môj život asi do
         minulého roku, kedy som začal riešiť viac prácu a školu (diplomovú
         prácu). Koncerty a hranie každý víkend nebolo to čo som hľadal a
         rozhodol som sa toto hobby na chvíľu pozastaviť. Už dlhšie som sa
         pohrával s myšlienkou robiť video, bavilo ma editovať fotky a hrať sa s
         farbami, tak to začalo asi u viacerých. Keď som sa rozhodol konečne si
-        kúpiť moju prvú Sony bezzrkadlovku, objavil som další passion, ktorému
-        som ihneď prepadol. Môj proces hľadania sa v tomto svete začal hudbou a
-        teraz pokračuje videom, nehovorím, že tu aj skončím ale na teraz to je
-        moja cesta. Dnes končím posledný semester vysokej školy, riešim prestup
-        z brigádnika na full-time a po víkendoch rád točím a strihám video.
-        Veľká časť video produkcie, ktorá ma zhltla je samotné farbenie a color
-        grade. Ako mi to ide môžeš pozrieť aj dole v projektoch, ak tam toho moc
-        nie je, znamená že tu si dosť skoro, a ešte som toho veľa nestihol, tak
-        vitaj :). <br /> Verím, že toho bude čoskoro viac.
+        kúpiť moju prvú Sony kameru, objavil som další passion, ktorému som
+        ihneď prepadol. Môj proces hľadania sa v tomto svete začal hudbou a
+        teraz pokračuje videom. Nehovorím, že tu aj skončím, ale na teraz to je
+        moja cesta. Momentálne končím posledný semester vysokej školy, riešim
+        prestup z brigádnika na full-time a po víkendoch rád točím a strihám
+        video. Veľká časť video produkcie, ktorá ma zhltla, je samotné farbenie
+        a color grade. Ako mi to ide, môžeš pozrieť aj dole v projektoch. Ak tam
+        toho moc nie je, znamená že tu si dosť skoro, a ešte som toho veľa
+        nestihol, tak vitaj :). <br /> Verím, že toho bude čoskoro viac.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
