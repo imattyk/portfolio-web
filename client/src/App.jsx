534010d8b63cc69e12ed7c14f29cdb90c366c393
Matej Komlóši <matej.komlosi@gmail.com>
2023-04-20 15:38:55 +0200

    prettier setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "./App.scss";
-import HttpsRedirect from "react-https-redirect";
-import Theme from "./styles/theme";
-import Hero from "./components/Hero/Hero";
-import Projects from "./components/Projects/Projects";
-import Technologies from "./components/Technologies/Technologies";
-import Timeline from "./components/TimeLine/TimeLine";
-import { Layout } from "./layout/Layout";
-import { Section, SectionDivider } from "./styles/GlobalComponents";
-import Acomplishments from "./components/Acomplishments/Acomplishments";
-import BgAnimation from "./components/BackgrooundAnimation/BackgroundAnimation";
-import DoraVideo from "./components/Video/web_background_1.mp4";
-import { HashLink } from "react-router-hash-link";
+import React from 'react';
+import './App.scss';
+import HttpsRedirect from 'react-https-redirect';
+import Theme from './styles/theme';
+import Hero from './components/Hero/Hero';
+import Projects from './components/Projects/Projects';
+import Technologies from './components/Technologies/Technologies';
+import Timeline from './components/TimeLine/TimeLine';
+import { Layout } from './layout/Layout';
+import { Section, SectionDivider } from './styles/GlobalComponents';
+import Acomplishments from './components/Acomplishments/Acomplishments';
+import BgAnimation from './components/BackgrooundAnimation/BackgroundAnimation';
+import DoraVideo from './components/Video/web_background_1.mp4';
+import { HashLink } from 'react-router-hash-link';
 
 function App() {
   return (
@@ -24,13 +24,13 @@ function App() {
             muted
             id="bg_video"
             style={{
-              position: "absolute",
-              width: "100%",
-              left: "50%",
-              height: "100%",
-              objectFit: "cover",
-              transform: "translate(-50%, 0%)",
-              zIndex: "-1",
+              position: 'absolute',
+              width: '100%',
+              left: '50%',
+              height: '100%',
+              objectFit: 'cover',
+              transform: 'translate(-50%, 0%)',
+              zIndex: '-1',
             }}
           >
             <source src={DoraVideo} type="video/mp4"></source>
