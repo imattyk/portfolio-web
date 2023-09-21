9ebcd4f0c8749137cc72b56070ad7d5bda1b9ff9
Matej Komlóši <matej.komlosi@gmail.com>
2023-09-21 18:09:42 +0200

    anicka dusicka

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,53 +1,65 @@
 export const projects = [
   {
-    title: "Dora - Z východnej dievčatá",
+    title: 'Dora - Z východnej dievčatá',
     description:
-      "Nápad, ktorý vznikol medzi mnou a hlavným gitaristom kapely, sa rozvil do môjho prvého videoklipu. Za pomoc opäť ďakujem Tiborovi Seidlovi, gitaristovi kapely Dora za túto možnosť a výsledok verím, že stál za to. Lokáciu a komparz zabezpečila kapela, video produkciu a edit som zabezpečil ja. Môj prvý projekt, verím, že nie posledný :)",
-    image: "/images/dora_z_vychodnej.jpg",
+      'Nápad, ktorý vznikol medzi mnou a hlavným gitaristom kapely, sa rozvil do môjho prvého videoklipu. Za pomoc opäť ďakujem Tiborovi Seidlovi, gitaristovi kapely Dora za túto možnosť a výsledok verím, že stál za to. Lokáciu a komparz zabezpečila kapela, video produkciu a edit som zabezpečil ja. Môj prvý projekt, verím, že nie posledný :)',
+    image: '/images/dora_z_vychodnej.jpg',
     urls: [
-      "https://www.youtube.com/watch?v=Ku6UwPt9Ggg",
-      "https://www.kapeladora.sk/",
+      'https://www.youtube.com/watch?v=Ku6UwPt9Ggg',
+      'https://www.kapeladora.sk/',
     ],
-    buttons: ["Video", "Kapela Dora"],
+    buttons: ['Video', 'Kapela Dora'],
     id: 0,
   },
   {
-    title: "Dora - Nepovedz dievča materi",
+    title: 'Dora - Nepovedz dievča materi',
     description:
-      "Druhá spolupráca s chalanmi z kapely Dora. Tento krát sme kapelné zábery natáčali v kaštieli Esterházy Galanta a musím povedať, že sa velice vydarili. Ďakujem a teším sa na ďalšiu spoluprácu.",
-    image: "/images/dora_nepovedz.jpg",
+      'Druhá spolupráca s chalanmi z kapely Dora. Tento krát sme kapelné zábery natáčali v kaštieli Esterházy Galanta a musím povedať, že sa velice vydarili. Ďakujem a teším sa na ďalšiu spoluprácu.',
+    image: '/images/dora_nepovedz.jpg',
     urls: [
-      "https://www.youtube.com/watch?v=LJQhu35Kcbo",
-      "https://www.kapeladora.sk/",
-      "https://www.youtube.com/watch?v=9PyIbp2z6q4",
+      'https://www.youtube.com/watch?v=LJQhu35Kcbo',
+      'https://www.kapeladora.sk/',
+      'https://www.youtube.com/watch?v=9PyIbp2z6q4',
     ],
-    buttons: ["Video", "Kapela Dora", "Behind the scenes"],
+    buttons: ['Video', 'Kapela Dora', 'Behind the scenes'],
     id: 1,
   },
   {
-    title: "Zakázaný Prístup - Valentín",
+    title: 'Zakázaný Prístup - Valentín',
     description:
       'S chalanmi z kapely Zakázaný Prístup sme dali dokopy tématický klip pre ich novú pesničku "Valentín". Točenie celého klipu v ich skúšobni nám dalo možnosť vyhrať sa so scénami a v punkovom nádychu sa dobre zabaviť pred kamerou ale aj mimo :D.',
-    image: "/images/valentin.jpg",
+    image: '/images/valentin.jpg',
     urls: [
-      "https://www.youtube.com/watch?v=QtalzjxlQPQ",
-      "https://www.instagram.com/zakazany_pristup/",
-      "https://www.youtube.com/watch?v=Ac_mwioimKM",
+      'https://www.youtube.com/watch?v=QtalzjxlQPQ',
+      'https://www.instagram.com/zakazany_pristup/',
+      'https://www.youtube.com/watch?v=Ac_mwioimKM',
     ],
-    buttons: ["Video", "Kapela Zakázaný Prístup", "Behind the scenes"],
+    buttons: ['Video', 'Kapela Zakázaný Prístup', 'Behind the scenes'],
     id: 2,
   },
   {
-    title: "Origin Lost - Awakening",
+    title: 'Origin Lost - Awakening',
     description:
-      "Djent a metal je môjmu srdcu veľmi blízky. Pri tomto projekte s kapelkou Origin Lost som sa teda cítil velice komfortne a okrem super spolupráce som si užil aj skvelú originálnu muziku a z toho čo som počul, ich nové EPčko si rovno dávam do playlistu na spotify.",
-    image: "/images/origin_lost.jpg",
+      'Djent a metal je môjmu srdcu veľmi blízky. Pri tomto projekte s kapelkou Origin Lost som sa teda cítil velice komfortne a okrem super spolupráce som si užil aj skvelú originálnu muziku a z toho čo som počul, ich nové EPčko si rovno dávam do playlistu na spotify.',
+    image: '/images/origin_lost.jpg',
     urls: [
-      "https://www.youtube.com/watch?v=E8DW85RIXd4",
-      "https://www.instagram.com/origin_lost/",
+      'https://www.youtube.com/watch?v=E8DW85RIXd4',
+      'https://www.instagram.com/origin_lost/',
     ],
-    buttons: ["Video", "Kapela Origin Lost"],
+    buttons: ['Video', 'Kapela Origin Lost'],
     id: 3,
+  },
+  {
+    title: 'Dora - Anička dušička',
+    description:
+      'Ďalší projektík s kapelou Dora, musím povedať, že tento klip sa podaril.',
+    image: '/images/anick_dusicka.jpg',
+    urls: [
+      'https://www.youtube.com/watch?v=3IO8u9utC6o',
+      'https://www.kapeladora.sk/',
+    ],
+    buttons: ['Video', 'Kapela Dora'],
+    id: 4,
   },
 ];
 
